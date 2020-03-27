b91a8d0db7e8b7b503f734802c645c33b97cd42e
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-03-27 13:27:10 +0100

    shtimi i regjistrimit ne databaz

--- PhpFiles/registerDatabaze.php
+++ PhpFiles/registerDatabaze.php
@@ -10,8 +10,19 @@ $connnection=new mysqli($server,$usernameDB,$passwordDB,$databaze);
 if($connnection->connect_error){
 	die("gabim ne lidhjen ne databaze");
 } 
+
+
+
+
+
+
+
+$stmt = $connnection->prepare("INSERT INTO `users`( `name`, `password`, `username`, `email`, `phone_nr`, `birthday`, `gender`, `user_rights`, `surname`, `activationStatus`) VALUES (?,?,?,?,?,?,?,?,?,?)");
+$stmt->bind_param('ssssssssss',$emri,$password,$username,$email,$phone_nr,$birthday,$gender,$user_rights,$mbiemri,$statusi);
+
+
 $emri=$_POST['emri'];
-$password=$_POST["password"];
+$password=password_hash($_POST["password"], PASSWORD_DEFAULT);
 $username=$_POST["username"];
 $email=$_POST["email"];
 $phone_nr="0684935250";
@@ -21,17 +32,7 @@ $user_rights=3;
 $mbiemri=$_POST["mbiemri"];
 $statusi=false;
 
-
-
-
-$query="INSERT INTO `users`( `name`, `password`, `username`, `email`, `phone_nr`, `birthday`, `gender`, `user_rights`, `surname`, `activationStatus`) VALUES ('{$emri}','{$password}','{$username}','{$email}','{$phone_nr}','{$birthday}','{$gender}','{$user_rights}','{$mbiemri}','{$statusi}')";
-
-$connnection->query($query);
-
-
-
-
-
+$stmt->execute();
 
 
  ?>
