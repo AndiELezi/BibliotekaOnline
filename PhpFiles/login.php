8ef0a1670804b4c83798bb12db5356b77c8e6574
Artenc Ceruni <artenc.cerumi8891@gmaail.com>
2020-03-31 22:37:50 +0200

    logini

--- PhpFiles/login.php
+++ PhpFiles/login.php
@@ -1,3 +1,25 @@
-<?php
-echo "kjo do jet faqja loginit"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login WebPage</title>
+	<link rel="stylesheet" type="text/css" href="style1.css" >
+<body>
+	<div class="loginbox" >
+
+		<img src="kot.png" class="kot">
+		<h1>Login here</h1>
+		<form>
+			<p>Username</p>
+			<input type="text" name="" placeholder="Enter Username">
+			<p>Password</p>
+			<input type="Password" name="" placeholder="Enter Password">
+			<input type="submit" name="" value="Login">
+			</p>
+			<p class="prove">Dont have an account? <a href="Register.html">Register</a></p>
+			<a href="#">Lost your password?</a><br>
+		</form>
+	</div>
+	
+</body>
+</head>
+</html>
