bd647c2412af5d01c1a90d64010d285a5bedc398
ArditKallaku <kallaku.ardit@gmail.com>
2020-04-02 01:03:10 +0200

    check db, issue!

--- PhpFiles/login.php
+++ PhpFiles/login.php
@@ -3,7 +3,51 @@
 <head>
 	<title>Login</title>
 <body>
-	<form method="Post" action="Login.php">
+
+	<?php
+	$username="";
+	$password="";
+	$errUsername="";
+	$errPassword="";
+
+	function checkUsernameAvailability($usernameToCheck){
+		$server='localhost';
+		$usernameDB='root';
+		$passwordDB='';
+		$databaze="biblioteka";
+		$connnection=new mysqli($server,$usernameDB,$passwordDB,$databaze);
+
+		if($connnection->connect_error){
+			die("gabim ne lidhjen ne databaze");
+		} 
+
+		$sql="SELECT name FROM users WHERE username='{$usernameToCheck}'";
+		$result=$connnection->query($sql);
+		if($result->num_rows<1){
+			$errUsername="This user doesn't exist";
+		}
+	}
+
+		if(isset($_POST["submit"])){
+			if(!empty($_POST["username"])){
+				$username=$_POST["username"];
+
+				checkUsernameAvailability($username);
+			}
+			else{
+				$errUsername="Shkruani username";
+			}
+			if(!empty($_POST["password"])){
+				$password=$_POST["password"];
+			}
+			else{
+				$errPassword="Shkruani passwordin";
+			}
+
+		}
+	?>
+
+	<form method="post" action="login.php">
 		Username : <input type="text" id="username" name="username" value="<?php echo $username;?>" placeholder="username" ><span id="errUsername"><?php echo $errUsername;?> </span><br>
 		Password : <input type="text" id="password" name="password" value="<?php echo $password;?>" placeholder="password"><span id="errPassword"><?php echo $errPassword; ?></span><br>
 		<input type="submit" name="submit">
