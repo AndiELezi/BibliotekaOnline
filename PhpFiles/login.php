ecae6863a0a503337a4152a13c80920e8da6ab98
ArditKallaku <kallaku.ardit@gmail.com>
2020-04-02 14:13:34 +0200

    login credenitals

--- PhpFiles/login.php
+++ PhpFiles/login.php
@@ -10,7 +10,7 @@
 	$errUsername="";
 	$errPassword="";
 
-	function checkUsernameAvailability($usernameToCheck){
+	function checkCredentials($usernameToCheck,$passwordToCheck){
 		global $errUsername;
 		global $errPassword;
 		$server='localhost';
@@ -23,43 +23,44 @@
 			die("gabim ne lidhjen ne databaze");
 		} 
 
-		$sql="SELECT name FROM users WHERE username='{$usernameToCheck}'";
-		$result=$connnection->query($sql);
-		if($result->num_rows<1){
-			$errUsername="This user doesn't exist";
-		}
-
 		$sql="SELECT password FROM users WHERE username='{$usernameToCheck}'";
 		$result=$connnection->query($sql);
-		
 		if($result->num_rows>0){
-			$password=$result->fetch_assoc();
-			$errPassword=$password;
+				$passwordResult=$result->fetch_assoc();
+				$passwordResult="'".$passwordResult["password"]."'";
+				if(!password_verify($passwordToCheck, $passwordResult)){
+					$errPassword="Incorrect password";
+				}
+		}
+		else{
+			$errUsername="This user doesn't exist";
 		}
 	}	
 
 		if(isset($_POST["submit"])){
+			
 			if(!empty($_POST["username"])){
 				$username=$_POST["username"];
 
-				checkUsernameAvailability($username);
+				if(!empty($_POST["password"])){
+					$password=$_POST["password"];
+					checkCredentials($username, $password);
+				}
+				else{
+					$errPassword="Shkruani passwordin";
+				}
 			}
 			else{
 				$errUsername="Shkruani username";
 			}
-			if(!empty($_POST["password"])){
-				$password=$_POST["password"];
-			}
-			else{
-				$errPassword="Shkruani passwordin";
-			}
+			
 
 		}
 	?>
 
 	<form method="post" action="login.php">
 		Username : <input type="text" id="username" name="username" value="<?php echo $username;?>" placeholder="username" ><span id="errUsername"><?php echo $errUsername;?> </span><br>
-		Password : <input type="text" id="password" name="password" value="<?php echo $password;?>" placeholder="password"><span id="errPassword"><?php echo $errPassword; ?></span><br>
+		Password : <input type="password" id="password" name="password" value="<?php echo $password;?>" placeholder="password"><span id="errPassword"><?php echo $errPassword; ?></span><br>
 		<input type="submit" name="submit">
 		<p class="Duhet Rregullu"> <a href="Register.php">Register</a></p> <!-- Ket e shkrujta njeher kshu se di si do e bejm -->
 			<a href="Forgot.html">Lost your password?</a><br> <!--kjo duhet tna coj ne nje faqe qe kerko email -->
