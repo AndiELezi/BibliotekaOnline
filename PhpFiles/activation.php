70c23edbdf45e6c97ffc9da2d8342ccc997bd84c
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-04-02 17:37:29 +0200

    ndryshim ne mail activation

--- PhpFiles/activation.php
+++ PhpFiles/activation.php
@@ -4,17 +4,17 @@ $server='localhost';
 $usernameDB='root';
 $passwordDB='';
 $databaze="biblioteka";
-$emri=$_GET["name"];
+$username=$_GET["username"];
 $securityString=$_GET["securityStringMail"];
 $connnection=new mysqli($server,$usernameDB,$passwordDB,$databaze);
 
 if($connnection->connect_error){
 	die("gabim ne lidhjen ne databaze");
 } 
-$sql="SELECT name FROM users WHERE name='{$emri}' AND securityString='{$securityString}'";
+$sql="SELECT name FROM users WHERE username='{$username}' AND securityString='{$securityString}'";
 $result=$connnection->query($sql);
 if($result->num_rows>0){
-	$sql="UPDATE `users` SET `activationStatus`=true WHERE name='{$emri}' AND securityString='{$securityString}'";
+	$sql="UPDATE `users` SET `activationStatus`=true WHERE username='{$username}' AND securityString='{$securityString}'";
 	$connnection->query($sql);
 	echo "activation successful";
 }
