c11f44e9a49e66cb1f9e7baf3786eff9829175af
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-04-14 17:10:35 +0200

    profilePhotoUpload

--- PhpFiles/profile.php
+++ PhpFiles/profile.php
@@ -1,4 +1,25 @@
 <?php 
+session_start();
 
-echo "hello";
+		
+$server='localhost';
+		$usernameDB='root';
+		$passwordDB='';
+		$databaze="biblioteka";
+		$connection=new mysqli($server,$usernameDB,$passwordDB,$databaze);
+
+		if($connection->connect_error){
+			die("gabim ne lidhjen ne databaze");
+		} 
+		$username=$_SESSION["username"];
+		$sql="SELECT `name`,`surname`,`points`,`profile_photo`,`email`,`mobile`,`birthday`,`gender` FROM users WHERE username='{$username}'";
+		$result=$connection->query($sql);
+				$user = $result->fetch_assoc();
+
  ?>
+ <form  action="uploadProfilePhoto.php" method="POST" enctype="multipart/form-data">
+ 	
+ 	<input type="file" name="profilePhoto">
+ 	<input type="submit" value="change" name="change">
+
+ </form>
---
+++ PhpFiles/uploadProfilePhoto.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+$server='localhost';
+		$usernameDB='root';
+		$passwordDB='';
+		$databaze="biblioteka";
+		$connection=new mysqli($server,$usernameDB,$passwordDB,$databaze);
+
+		if($connection->connect_error){
+			die("gabim ne lidhjen ne databaze");
+		} 
+if(isset($_POST["change"])){
+$file=$_FILES["profilePhoto"];
+$fileName=$_FILES["profilePhoto"]["name"];
+$fileTempName=$_FILES["profilePhoto"]["tmp_name"];
+$fileSize=$_FILES["profilePhoto"]["size"];
+$fileError=$_FILES["profilePhoto"]["error"];
+$fileType=$_FILES["profilePhoto"]["type"];
+
+//ndajm fjalen kudo ku ka pika
+$fileNameSeparated=explode(".", $fileName);
+//gjejm prpapashtesen psh jpg  etc
+$fileExtension=strtolower(end($fileNameSeparated));
+
+//nje array me t gjitha tipet e mundshme t fileve t lejuara
+$allowed=array('jpg','jpeg','png');
+if(in_array($fileExtension,$allowed)){
+	if($fileError===0){
+		$username=$_SESSION["username"];
+		$fileNewName="\\".$_SESSION["username"].".".$fileExtension;
+		$fileDestination='C:\xampp\htdocs\BibliotekaOnline\UserImages'.$fileNewName;
+		$sql="SELECT profile_photo FROM users WHERE `username`='{$username}'";
+		$result=$connection->query($sql);
+		$profileArray=$result->fetch_assoc();
+		if(strcmp("default.jpg", $profileArray["profile_photo"])!==0){
+				unlink($fileDestination);
+		}
+		move_uploaded_file($fileTempName, $fileDestination);
+		$profilePhoto=$_SESSION["username"].".".$fileExtension;
+		$sql="UPDATE users SET `profile_photo`='{$profilePhoto}' WHERE `username`='{$username}'";
+		$connection->query($sql);
+		header("Location:profile.php");
+
+		}
+	else{
+		//dicka nqs ndodh gabim n uplodim;
+		}	
+	}
+
+else{
+		//dicka nqs ndodh gabim n formatin e fotos;
+	}
+
+}
+
+
+
+  ?>
