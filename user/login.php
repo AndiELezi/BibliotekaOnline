0104c7d6ddc5c1e628fce51e39e5566e3388b868
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-04-15 13:02:40 +0200

    small chng

--- user/login.php
+++ user/login.php
@@ -86,8 +86,8 @@ session_start();
 		Username : <input type="text" id="username" name="username" value="<?php echo $username;?>" placeholder="username" ><span id="errUsername"><?php echo $errUsername;?> </span><br>
 		Password : <input type="password" id="password" name="password" value="<?php echo $password;?>" placeholder="password"><span id="errPassword"><?php echo $errPassword; ?></span><br>
 		<input type="submit" name="submit">
-		<p class="Duhet Rregullu"> <a href="Register.php">Register</a></p> <!-- Ket e shkrujta njeher kshu se di si do e bejm -->
-			<a href="Forgot.html">Lost your password?</a><br> <!--kjo duhet tna coj ne nje faqe qe kerko email -->
+		<p class="Duhet Rregullu"> <a href="register.php">Register</a></p> <!-- Ket e shkrujta njeher kshu se di si do e bejm -->
+			<a href="forgotPassword.php">Lost your password?</a><br> <!--kjo duhet tna coj ne nje faqe qe kerko email -->
 	</form>
 </body>
 </head>
