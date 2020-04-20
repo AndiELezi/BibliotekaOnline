0bc5b88506ecdf33e8b3b55e20ff8a4dd0bd0f5a
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-04-20 20:43:08 +0200

    ndryshime n search.php

--- user/search.php
+++ user/search.php
@@ -7,19 +7,22 @@ $searchQuery=$_GET["searchQuery"];
 
 //nqs ka kerkuar per titull
 
-if(strcmp($_GET["filter"],"title")==0){
- $sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\"";
- $offlineBooksResult=$connection->query($sql);
- $sql="SELECT id,cover_photo,title FROM online_books WHERE title LIKE \"%{$searchQuery}%\"";
- $result=$connection->query($sql);
- $online_books=$result->fetch_assoc();
- echo $online_books["title"];
-}
+	if(strcmp($_GET["filter"],"title")==0){
+ 	$sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\"";
+ 	$offlineBooksResult=$connection->query($sql);
+ 	$sql="SELECT id,cover_photo,title FROM online_books WHERE title LIKE \"%{$searchQuery}%\"";
+ 	$online_books=$connection->query($sql);
+
+	}	
 
 // nqs ka kekuar per autor (dhe user po ne te njejten vend po e vendos)
-if(strcmp($_GET["filter"],"author")==0){
-//ktu e kam len
-}
+	else if(strcmp($_GET["filter"],"author")==0){
+		$sql="SELECT ISBN,cover_photo,title FROM v_author_book WHERE full_name LIKE \"%{$searchQuery}%\"";
+		$offlineBooksResult=$connection->query($sql);
+		$sql="SELECT book_id,cover_photo,title FROM v_user_online_books WHERE name LIKE \"%{$searchQuery}%\" OR surname LIKE \"%{$searchQuery}%\"";
+		$onlineBooksResult=$connection->query($sql);
+
+	}
 
 
 
