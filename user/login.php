98c23f246de636ff037a70b1521f1b58ba67dc2f
ArditKallaku <kallaku.ardit@gmail.com>
2020-05-02 18:22:44 +0200

    authentication form

--- user/login.php
+++ user/login.php
@@ -98,7 +98,6 @@ session_start();
 		<a href="register.php">Register</a> <br><!-- Ket e shkrujta njeher kshu se di si do e bejm -->
 		<a href="forgotPassword.php">Lost your password?</a> <!--kjo duhet tna coj ne nje faqe qe kerko email -->
 	</form>
-
 	<!-- jquery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<!-- script qe shton classen has-content per inputet qe nuk jane bosh -->
