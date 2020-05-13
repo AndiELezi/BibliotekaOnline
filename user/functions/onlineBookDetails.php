29961cbfad5688745d8b118b0678b411c50ceb8a
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-05-13 13:34:04 +0200

    pergatitje per review description

--- user/functions/onlineBookDetails.php
+++ user/functions/onlineBookDetails.php
@@ -2,6 +2,7 @@
 
 
 $id=$_GET["id"];
+	$userId=$userData["id"];
 	$sql="SELECT * from online_books where id='{$id}'";
 	$resultOnlineBook=$connection->query($sql);
 	$online_books=$resultOnlineBook->fetch_assoc();
@@ -12,7 +13,48 @@ $id=$_GET["id"];
 	$image_path_online_books="/BibliotekaOnline/images/onlineBooks/";
 	$image_path_online_books.=$online_books["cover_photo"];
 	echo "<img src='$image_path_online_books'>"."<br>";
-	echo "id ".$online_books["id"]."<br>". "titulli ".$online_books["title"]."<br>"."category: ";
+
+/***************************************/
+$sql="SELECT * FROM review WHERE user_id='{$userId}' AND book_id='{$id}'";
+	$reviewResult=$connection->query($sql);
+
+	if($reviewResult->num_rows>0){
+			$reviewResultData=$reviewResult->fetch_assoc();
+			$liked=$reviewResultData["liked"];
+			$favourite=$reviewResultData["favourite"];
+
+			if($liked==0){
+				echo "<img id='like' onclick=\"likePressed('onlineBook','".$id."')\"  src='/BibliotekaOnline/images/app/likeEmpty.png'>";
+
+			}
+			else if($liked==1){
+				echo "<img id='like' onclick=\"likePressed('onlineBook','".$id."')\"  src='/BibliotekaOnline/images/app/likeFilled.png'>";
+			}
+
+			if($favourite==0){
+				echo "<img id='favourite' onclick=\"favouritePressed('onlineBook','".$id."')\" src='/BibliotekaOnline/images/app/favouriteEmpty.png'>";
+
+			}
+			else if($favourite==1){
+				echo "<img id='favourite' onclick=\"favouritePressed('onlineBook','".$id."')\"  src='/BibliotekaOnline/images/app/favouriteFilled.png'>";
+			}
+
+
+
+	}
+	else{
+
+		echo "<img id='like' onclick=\"likePressed('onlineBook','".$id."')\" src='/BibliotekaOnline/images/app/likeEmpty.png'>";
+	 echo "<img id='favourite' onclick=\"favouritePressed('onlineBook','".$id."')\" src='/BibliotekaOnline/images/app/favouriteEmpty.png'>";
+	 echo "<br>";
+
+	}
+/***************************************/
+
+
+
+
+	echo "<br>id ".$online_books["id"]."<br>". "titulli ".$online_books["title"]."<br>"."category: ";
 	while($online_books_Category=$onlineBookCategory->fetch_assoc()){
 		echo $online_books_Category["c_description"]." ";
 	}
