cf85b48328f0ffa61c03b515d4109f9f510bc621
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-05-20 15:28:39 +0200

    perfundimi i book reservations per librarianin

---
+++ user/librarian/delayedBookUsers.php
@@ -0,0 +1,47 @@
+<?php
+include '../functions/DBconnection.php';
+$date=date("Y-m-d");
+$pageNr=$_GET["pageNr"];
+$numberPerPage=2;
+$startPage=$pageNr*$numberPerPage-$numberPerPage;
+$sql="SELECT COUNT(*) as totali FROM book_reservation where returnTime<='{$date}' AND taken=1";
+$totalResult=$connection->query($sql);
+$totalResultFetched=$totalResult->fetch_assoc();
+$totali=$totalResultFetched["totali"];
+$mbetja=$totali%$numberPerPage;
+$nrFaqeve;
+if($mbetja>0){
+	$nrFaqeve=(($totali-$mbetja)/$numberPerPage)+1;
+}
+else{
+	$nrFaqeve=$totali/$numberPerPage;
+}
+$sql="SELECT users.id,users.username,users.name,users.surname,users.email,users.mobile,book_reservation.book_id,book_reservation.reservation_time,book_reservation.returnTime,book_reservation.delay_fine  FROM book_reservation INNER JOIN users on users.id=book_reservation.user_id where returnTime<='{$date}' AND taken=1 LIMIT $startPage,$numberPerPage";
+
+$result=$connection->query($sql);
+echo "<table>";
+echo "<tr><th>User Id</th><th>username</th><th>Emri</th><th>Mbiemri</th><th>email</th><th>Mobile</th><th>Book Isbn</th><th>Koha Rezervimit</th><th>Koha Kthimit</th><th>Gjoba</th></tr>";
+while ($i=$result->fetch_assoc()) {
+	echo "<tr><td>".$i["id"]."</td>"."<td>".$i["username"]."</td>"."<td>".$i["name"]."</td>"."<td>".$i["surname"]."</td>"."<td>".$i["email"]."</td>"."<td>".$i["mobile"]."</td>"."<td>".$i["book_id"]."</td>"."<td>".$i["reservation_time"]."</td>"."<td>".$i["returnTime"]."</td>"."<td>".$i["delay_fine"]."</td>"."</tr>";
+}
+echo "</table>";
+
+$i=1;
+
+while ($i <= $nrFaqeve) {
+	if($i==$pageNr){
+		echo "<a style='color:red' href='delayedBookUsers.php?pageNr=".$i."'>".$i."</a> ";
+	}
+	else{
+		echo "<a href='delayedBookUsers.php?pageNr=".$i."'>".$i."</a> ";
+	}
+
+	$i++;
+}
+
+
+
+  ?>
+
+
+
