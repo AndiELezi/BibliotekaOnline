14ca0c2fd447369dbd0e709f52afda9b27bd66fa
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-05-21 18:53:18 +0200

    ndryshime ne databaze

--- Databaza/biblioteka.sql
+++ Databaza/biblioteka.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2020 at 08:22 PM
+-- Generation Time: May 21, 2020 at 06:52 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `author` (
   `id` int(11) NOT NULL,
-  `full_name` varchar(30) NOT NULL,
+  `full_name` varchar(50) NOT NULL,
   `birthplace` varchar(30) NOT NULL,
   `birthday` date NOT NULL,
   `contact` varchar(50) NOT NULL
@@ -41,27 +40,14 @@ CREATE TABLE `author` (
 --
 
 INSERT INTO `author` (`id`, `full_name`, `birthplace`, `birthday`, `contact`) VALUES
-(1, 'a', 'a', '2020-04-01', 'a'),
-(8, 'b', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(9, 'c', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(10, 'd', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(11, 'e', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(12, 'f', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(13, 'g', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(14, 'h', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(15, 'j', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(16, 'k', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(17, 'l', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(18, 'm', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(19, 'n', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(20, 'o', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(21, 'p', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(22, 'q', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(23, 'r', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(24, 's', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(25, 't', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(26, 'u', 'nje vendlindje', '2020-05-01', 'nje kontakt'),
-(27, 'i', 'nje vendlindje', '2020-05-01', 'nje kontakt');
+(28, 'Sebastian Junger', 'America', '1962-01-17', 'unknown'),
+(29, 'Brian Harbert', 'America', '1947-06-29', 'unknown'),
+(30, 'Kevin J.Anderson', 'America', '0000-00-00', ' 35556255451'),
+(31, 'Suzane Collins', 'America', '1899-01-06', 'unknown'),
+(32, 'Veronica Roth', 'America', '1925-01-06', 'veronica@gmail.com'),
+(33, 'Stephenie Mayer', 'America', '1988-12-06', 'unknown'),
+(34, 'Thomas Reye', 'England', '1998-10-01', ' 35566854'),
+(35, 'Nicholas Sparks', 'France', '1700-12-06', 'unknown');
 
 -- --------------------------------------------------------
 
@@ -71,14 +57,14 @@ INSERT INTO `author` (`id`, `full_name`, `birthplace`, `birthday`, `contact`) VA
 
 CREATE TABLE `book` (
   `ISBN` varchar(30) NOT NULL,
-  `title` varchar(50) NOT NULL,
+  `title` varchar(75) NOT NULL,
   `publication_year` date NOT NULL,
   `publish_house` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   `reservation_points` int(11) NOT NULL,
-  `cover_photo` varchar(100) DEFAULT NULL,
-  `description` varchar(200) DEFAULT NULL,
+  `cover_photo` varchar(150) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
   `likes` int(11) DEFAULT NULL,
   `monthly_likes` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -88,25 +74,16 @@ CREATE TABLE `book` (
 --
 
 INSERT INTO `book` (`ISBN`, `title`, `publication_year`, `publish_house`, `quantity`, `price`, `reservation_points`, `cover_photo`, `description`, `likes`, `monthly_likes`) VALUES
-('1', 'atest', '2000-12-12', 1, 2, 10, 50, 'a.jpg', 'blablabla', 0, 0),
-('10', 'Book10', '2014-12-06', 1, 2, 1000, 0, 'Book101589023306.png', 'book10 description', 0, 0),
-('2', 'A', '2001-12-12', 1, 1, 10, 0, 'b.jpg', 'blablabla', 3, 0),
-('3', 'A', '2003-12-12', 1, 1, 10, 20, 'c.jpg', 'blablabla', 12, 0),
-('4', 'A', '1998-01-01', 1, 1, 10, 20, 'd.jpg', 'blablabla', 14, 0),
-('5', 'A', '2005-12-12', 1, 1, 10, 100, 'e.jpg', 'blablabla', 18, 0),
-('6', 'A', '2006-12-12', 1, 1, 10, 0, 'f.jpg', 'blablabla', 23, 0),
-('8', 'banana', '2020-04-01', 1, 1, 12, 12, 'banana.jpg', 'asdasdasd', 10, 0),
-('9', 'hello', '2020-04-01', 1, 10, 10, 10, 'hello.jpg', 'sdaasda', 10, 0),
-('Book1', 'Book1', '1998-12-06', 2, 9, 1000, 0, 'Book11589022900.png', 'book1 description', 0, 0),
-('Book2', 'Book2', '1995-12-06', 2, 1, 1000, 0, 'Book21589022923.png', 'book2 description', 0, 0),
-('Book3', 'Book3', '1800-12-06', 2, 1, 1000, 0, 'Book31589022993.png', 'book3 description', 0, 0),
-('Book4', 'Book4', '1756-12-06', 2, 1, 1000, 0, 'Book41589023027.png', 'book4 description', 0, 0),
-('Book5', 'Book5', '2012-12-06', 2, 1, 1000, 0, 'Book51589023064.png', 'book5 description', 0, 0),
-('Book6', 'Book6', '2003-12-06', 2, 1, 1000, 0, 'Book61589023162.png', 'book6 description', 0, 0),
-('Book7', 'Book7', '1689-12-06', 2, 1, 1000, 0, 'Book71589023187.png', 'book7 description', 0, 0),
-('Book8', 'Book8', '1869-12-06', 2, 1, 1000, 0, 'Book81589023224.png', 'book8 description', 0, 0),
-('Book9', 'Book9', '1999-12-06', 2, 1, 1000, 0, 'Book91589023272.png', 'book9 description', 0, 0),
-('njeIsbn1234', 'bookTest', '1996-12-06', 1, 1, 1000, 0, 'bookTest1589190133.png', 'nj test', 0, 0);
+('9780060742690', 'A Death in Belmont', '2007-02-01', 10, 26, 15, 0, 'ADeathinBelmont1590075314.jpg', 'The quiet suburb of Belmont, Massacuusetts, is in the grip of fear. The Boston Strangler murders have taken place nearby, and now there is another shocking sex crime, right in Belmont. The victim is Bessie Goldberg, a middle-aged woman who had hired a cleaning man to help out around the house on that fall day in 1963. He is a black man named Roy Smith. He did the appointed chores, collected his money and left a receipt on the kitchen table. Neighbors will say that he looked furtive when he walked down the street, that he was in a hurry, that he stopped to buy cigarettes, that he looked over his shoulder. They didn\'t see a black man in Belmont very often, so, of course, they noticed him. So the story went, and on these slender threads, and his own checkered history, Roy Smith is convicted of the Belmont murder and sent to prison.', 0, 0),
+('9780061013515', 'The Perfect Storm: A True Story of Men Against the Sea', '2001-02-01', 8, 50, 4, 10, 'ThePerfectStormATrueStoryofMenAgainsttheSea1590074052.jpg', 'October 1991. It was \"the perfect storm\"--a tempest that may happen only once in a century--a nor\'easter created by so rare a combination of factors that it could not possibly have been worse. Creating waves ten stories high and winds of 120 miles an hour, the storm whipped the sea to inconceivable levels few people on Earth have ever witnessed. Few, except the six-man crew of the Andrea Gail, a commercial fishing boat tragically headed towards its hellish center.', 0, 0),
+('9780062345219', 'Four: A Divergent Collection', '2015-01-05', 13, 3, 20, 0, 'FourADivergentCollection1590077852.jpg', 'Two years before Beatrice Prior made her choice, the sixteen-year-old son of Abnegation\'s faction leader did the same. Tobias\'s transfer to Dauntless is a chance to begin again. Here, he will not be called the name his parents gave him. Here, he will not let fear turn him into a cowering child.', 0, 0),
+('9780316024969', 'New Moon (The Twilight Saga)', '2015-01-05', 14, 20, 12, 0, 'NewMoonTheTwilightSaga1590078296.jpg', 'For Bella Swan, there is one thing more important than life itself: Edward Cullen. But being in love with a vampire is even more dangerous than Bella could ever have imagined. Edward has already rescued Bella from the clutches of one evil vampire, but now, as their daring relationship threatens all that is near and dear to them, they realize their troubles may be just beginning.', 0, 0),
+('9780316160209', 'Eclipse (Twilight)', '2017-01-05', 14, 20, 12, 20, 'EclipseTwilight1590078608.jpg', 'As Seattle is ravaged by a string of mysterious killings and a malicious vampire continues her quest for revenge, Bella once again finds herself surrounded by danger. In the midst of it all, she is forced to choose between her love for Edward and her friendship with Jacob-knowing that her decision has the potential to ignite the ageless struggle between vampire and werewolf.', 0, 0),
+('9780316387842', 'The Chemist', '1888-03-05', 14, 20, 25, 50, 'TheChemist1590079060.jpg', 'She used to work for the U.S. government, but very few people ever knew that. An expert in her field, she was one of the darkest secrets of an agency so clandestine it doesn\'t even have a name. And when they decided she was a liability, they came for her without warning.', 0, 0),
+('9780393010466', 'Fire', '2001-02-01', 9, 20, 12, 0, 'Fire1590074639.jpg', 'The events explored in Fire focus on \"people confronting situations that could easily destroy them,\" and as he demonstrated in The Perfect Storm, Sebastian Junger is skilled at breaking such situations down to their core elements. In this exciting book, he reports on raging forest fires in the Western U.S, war zones in Kosovo and Afghanistan, the deadly diamond trade in Sierra Leone, the plight of travelers kidnapped by guerrillas in Kashmir, the last living whale harpooner on the Caribbean isla', 0, 0),
+('9780439023481', 'The Hunger Games', '2014-01-05', 12, 15, 20, 0, 'TheHungerGames1590077438.jpg', 'In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.', 0, 0),
+('9780765322722', 'The winds of dune', '2009-01-05', 11, 15, 10, 0, 'TORBooks1590076345.jpg', 'With their usual skill, Brian Herbert and Kevin Anderson have taken ideas left behind by Frank Herbert and filled them with living characters and a true sense of wonder. Where Paul of Dune picked up the saga directly after the events of Dune, The Winds of Dune begins after the events of Dune Messiah.', 0, 0),
+('9781455520633', 'The Longest Ride', '1898-03-05', 15, 20, 25, 25, 'TheLongestRide1590079780.jpg', 'From the dark days of WWII to present-day North Carolina, this New York Times bestseller shares the lives of two couples overcoming destructive secrets and finding joy together', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -124,19 +101,18 @@ CREATE TABLE `book_author` (
 --
 
 INSERT INTO `book_author` (`book_id`, `author_id`) VALUES
-('1', 1),
-('10', 17),
-('2', 1),
-('Book1', 8),
-('Book2', 8),
-('Book3', 9),
-('Book4', 9),
-('Book5', 11),
-('Book6', 13),
-('Book7', 13),
-('Book8', 27),
-('Book9', 18),
-('njeIsbn1234', 8);
+('9780060742690', 28),
+('9780061013515', 28),
+('9780062345219', 32),
+('9780316024969', 33),
+('9780316160209', 33),
+('9780316387842', 33),
+('9780316387842', 34),
+('9780393010466', 28),
+('9780439023481', 31),
+('9780765322722', 29),
+('9780765322722', 30),
+('9781455520633', 35);
 
 -- --------------------------------------------------------
 
@@ -154,43 +130,26 @@ CREATE TABLE `book_categories` (
 --
 
 INSERT INTO `book_categories` (`book_id`, `category_id`) VALUES
-('1', 1),
-('1', 2),
-('1', 3),
-('10', 1),
-('10', 7),
-('2', 1),
-('2', 5),
-('3', 4),
-('4', 4),
-('4', 6),
-('5', 1),
-('6', 3),
-('8', 5),
-('9', 6),
-('Book1', 2),
-('Book1', 7),
-('Book2', 2),
-('Book2', 7),
-('Book3', 2),
-('Book3', 5),
-('Book3', 7),
-('Book4', 2),
-('Book4', 5),
-('Book4', 7),
-('Book5', 1),
-('Book5', 2),
-('Book5', 7),
-('Book6', 1),
-('Book6', 2),
-('Book6', 7),
-('Book7', 1),
-('Book7', 2),
-('Book8', 1),
-('Book8', 2),
-('Book8', 7),
-('Book9', 2),
-('njeIsbn1234', 2);
+('9780060742690', 3),
+('9780060742690', 5),
+('9780061013515', 1),
+('9780061013515', 3),
+('9780061013515', 5),
+('9780062345219', 1),
+('9780062345219', 4),
+('9780062345219', 6),
+('9780316024969', 2),
+('9780316160209', 2),
+('9780316387842', 3),
+('9780393010466', 5),
+('9780439023481', 1),
+('9780439023481', 4),
+('9780439023481', 5),
+('9780439023481', 6),
+('9780765322722', 1),
+('9780765322722', 6),
+('9781455520633', 2),
+('9781455520633', 5);
 
 -- --------------------------------------------------------
 
@@ -336,13 +295,6 @@ CREATE TABLE `book_reservation` (
   `taken` tinyint(4) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `book_reservation`
---
-
-INSERT INTO `book_reservation` (`user_id`, `book_id`, `reservation_time`, `returnTime`, `delay_fine`, `taken`) VALUES
-(15, 'book8', '2020-05-18', '2020-05-20', 60, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -414,7 +366,7 @@ INSERT INTO `hall_structure` (`structure_id`, `library_hall`, `row_numbers`, `se
 
 CREATE TABLE `library_halls` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
+  `name` varchar(35) DEFAULT NULL,
   `capacity` int(11) NOT NULL,
   `open_seats` int(11) NOT NULL,
   `librarian_id` int(11) NOT NULL
@@ -437,12 +389,12 @@ INSERT INTO `library_halls` (`id`, `name`, `capacity`, `open_seats`, `librarian_
 CREATE TABLE `online_books` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `title` varchar(40) NOT NULL,
+  `title` varchar(75) NOT NULL,
   `publish_date` date NOT NULL,
   `likes` int(11) NOT NULL,
-  `cover_photo` varchar(100) DEFAULT NULL,
-  `description` varchar(200) DEFAULT NULL,
-  `book_path` varchar(100) DEFAULT NULL,
+  `cover_photo` varchar(150) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `book_path` varchar(150) DEFAULT NULL,
   `monthly_likes` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -495,7 +447,7 @@ INSERT INTO `online_books` (`id`, `user_id`, `title`, `publish_date`, `likes`, `
 
 CREATE TABLE `publish_house` (
   `id` int(11) NOT NULL,
-  `name` varchar(40) NOT NULL
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -503,8 +455,14 @@ CREATE TABLE `publish_house` (
 --
 
 INSERT INTO `publish_house` (`id`, `name`) VALUES
-(1, 'test'),
-(2, 'test2');
+(8, 'Harper Torch/HarperCollins'),
+(9, 'W. W. Norton & Company'),
+(10, 'Harper Perennial'),
+(11, 'TOR Books'),
+(12, 'Scholastic Press'),
+(13, 'Katherine Tegen Books'),
+(14, 'Little Brown'),
+(15, 'Grand Central Publishing');
 
 -- --------------------------------------------------------
 
@@ -550,7 +508,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `username`, `email`, `mobile`, `password`, `birthday`, `gender`, `points`, `user_rights`, `profile_photo`, `activationStatus`, `securityString`, `recoverPasswordToken`) VALUES
-(15, 'Andi', 'elezi', 'andi06121998', 'andielezi52@gmail.com', '+355684934250', '$2y$10$mCQdF6ERPR9K.oeifvG0DOdvXy./72eJgf6pma2BBpQuh74/N.J86', '1998-06-12', 'Male', 2010, 3, 'andi06121998gjfdioykhlsuwrepqta.jpg', 1, 'm3noby6hwfu80icjz2lktevsp71rdag4x5q9', NULL),
+(15, 'Andi', 'elezi', 'andi06121998', 'andielezi52@gmail.com', '+355684934250', '$2y$10$mCQdF6ERPR9K.oeifvG0DOdvXy./72eJgf6pma2BBpQuh74/N.J86', '1998-06-12', 'Male', 3510, 3, 'andi06121998gjfdioykhlsuwrepqta.jpg', 1, 'm3noby6hwfu80icjz2lktevsp71rdag4x5q9', NULL),
 (19, 'Ardit', 'Kallaku', 'silence', 'ardit.kallaku@fshnstudent.info', '+355681122334', '$2y$10$9F50q0p7pXOS.VQLkLoU1eXgKuKTfQRiC1I1vVDDZq13YyDpWXHV6', '2020-01-01', 'Male', 1004, 3, 'silenceypaglsjkohfwqetirdu.jpg', 1, '45g28ob1a9wkl03x6njprevqdtficzmhusy7', NULL),
 (20, 'Artenc', 'Cerumi', 'techno', 'artenc.cerumi8891@gmail.com', '+35556949250', '$2y$10$AaNUNpkRHb8RoZrDBw9TeOmfiw15Ff3f5DY87ZXHAzzVPjwHMhq1u', '2020-01-01', 'Male', 0, 3, NULL, 1, 'ds4lzngaqpm9e7516i2fcov8rxk30jytuhbw', NULL);
 
@@ -582,18 +540,18 @@ INSERT INTO `user_rights` (`id`, `description`) VALUES
 --
 CREATE TABLE `v_author_book` (
 `author_Id` int(11)
-,`full_name` varchar(30)
+,`full_name` varchar(50)
 ,`birthplace` varchar(30)
 ,`birthday` date
 ,`contact` varchar(50)
 ,`ISBN` varchar(30)
-,`title` varchar(50)
+,`title` varchar(75)
 ,`publication_year` date
 ,`quantity` int(11)
 ,`price` int(11)
 ,`reservation_points` int(11)
-,`cover_photo` varchar(100)
-,`description` varchar(200)
+,`cover_photo` varchar(150)
+,`description` varchar(1000)
 ,`likes` int(11)
 );
 
@@ -605,15 +563,15 @@ CREATE TABLE `v_author_book` (
 --
 CREATE TABLE `v_book_categories` (
 `ISBN` varchar(30)
-,`title` varchar(50)
+,`title` varchar(75)
 ,`c_description` varchar(50)
 ,`publication_year` date
 ,`publish_house` int(11)
 ,`quantity` int(11)
 ,`price` int(11)
 ,`reservation_points` int(11)
-,`cover_photo` varchar(100)
-,`description` varchar(200)
+,`cover_photo` varchar(150)
+,`description` varchar(1000)
 ,`likes` int(11)
 );
 
@@ -626,12 +584,12 @@ CREATE TABLE `v_book_categories` (
 CREATE TABLE `v_online_books_categories` (
 `id` int(11)
 ,`user_id` int(11)
-,`title` varchar(40)
+,`title` varchar(75)
 ,`c_description` varchar(50)
 ,`publish_date` date
 ,`likes` int(11)
-,`cover_photo` varchar(100)
-,`description` varchar(200)
+,`cover_photo` varchar(150)
+,`description` varchar(500)
 );
 
 -- --------------------------------------------------------
@@ -642,9 +600,9 @@ CREATE TABLE `v_online_books_categories` (
 --
 CREATE TABLE `v_publish_house_book` (
 `ISBN` varchar(30)
-,`title` varchar(50)
-,`cover_photo` varchar(100)
-,`name` varchar(40)
+,`title` varchar(75)
+,`cover_photo` varchar(150)
+,`name` varchar(100)
 );
 
 -- --------------------------------------------------------
@@ -666,11 +624,11 @@ CREATE TABLE `v_user_online_books` (
 ,`user_rights` int(11)
 ,`profile_photo` varchar(100)
 ,`book_id` int(11)
-,`title` varchar(40)
+,`title` varchar(75)
 ,`publish_date` date
 ,`likes` int(11)
-,`cover_photo` varchar(100)
-,`description` varchar(200)
+,`cover_photo` varchar(150)
+,`description` varchar(500)
 );
 
 -- --------------------------------------------------------
@@ -837,7 +795,7 @@ ALTER TABLE `user_rights`
 -- AUTO_INCREMENT for table `author`
 --
 ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `book_favourite`
@@ -879,7 +837,7 @@ ALTER TABLE `online_books`
 -- AUTO_INCREMENT for table `publish_house`
 --
 ALTER TABLE `publish_house`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `review`
@@ -913,15 +871,15 @@ ALTER TABLE `book`
 -- Constraints for table `book_author`
 --
 ALTER TABLE `book_author`
-  ADD CONSTRAINT `fk_author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`),
-  ADD CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`);
+  ADD CONSTRAINT `fk_author_id` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `book_categories`
 --
 ALTER TABLE `book_categories`
-  ADD CONSTRAINT `fk_book_ISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`),
-  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `fk_book_ISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `hall_structure`
