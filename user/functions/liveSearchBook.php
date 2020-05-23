94dd5365983200d37530dbd7ee34b0278e16dfbf
Andi <51848927+AndiELezi@users.noreply.github.com>
2020-05-23 15:31:37 +0200

    ndryshime

--- user/functions/liveSearchBook.php
+++ user/functions/liveSearchBook.php
@@ -4,20 +4,13 @@ if(!isset($_GET["searchQuery"])){
 	exit();
 }
 include 'DBconnection.php';
+$numberOfWantedResult=4;
 $searchQuery=$_GET["searchQuery"];
-$sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\"";
+$sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\" LIMIT $numberOfWantedResult";
 $result=$connection->query($sql);
 $bookResults="";
-$ok=true;
-$numberOfWantedResult=4;
-while ($numberOfWantedResult!=0) {
-	if($i=$result->fetch_assoc()){
-		$bookResults.="<a href='bookReservation.php?isbn=".$i["ISBN"]."'>"."<img src='/BibliotekaOnline/images/books/".$i["cover_photo"]."'>".$i["title"]."</a>"."<br>";
-		$numberOfWantedResult--;
-	}
-	else{
-		break;
-	}
+while ($i=$result->fetch_assoc()) {
+	$bookResults.="<a href='bookReservation.php?isbn=".$i["ISBN"]."'>"."<img src='/BibliotekaOnline/images/books/".$i["cover_photo"]."'>".$i["title"]."</a>"."<br>";
 
 }
 echo $bookResults;
--- user/functions/liveSearchHome.php
+++ user/functions/liveSearchHome.php
@@ -14,23 +14,23 @@ $searchQuery=$_GET["searchQuery"];
 //nqs ka kerkuar per titull
 
 	if(strcmp($_GET["filter"],"title")==0){
- 	$sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\"";
+ 	$sql="SELECT ISBN,cover_photo,title FROM book WHERE title LIKE \"%{$searchQuery}%\" LIMIT $desiredSearchNumber";
  	$offlineBooksResult=$connection->query($sql);
- 	$sql="SELECT id,cover_photo,title FROM online_books WHERE title LIKE \"%{$searchQuery}%\"";
+ 	$sql="SELECT id,cover_photo,title FROM online_books WHERE title LIKE \"%{$searchQuery}%\" LIMIT $desiredSearchNumber";
  	$onlineBooksResult=$connection->query($sql);
 
 	}	
 
 // nqs ka kekuar per autor (dhe user po ne te njejten vend po e vendos)
 	else if(strcmp($_GET["filter"],"author")==0){
-		$sql="SELECT ISBN,cover_photo,title FROM v_author_book WHERE full_name LIKE \"%{$searchQuery}%\"";
+		$sql="SELECT ISBN,cover_photo,title FROM v_author_book WHERE full_name LIKE \"%{$searchQuery}%\" LIMIT $desiredSearchNumber";
 		$offlineBooksResult=$connection->query($sql);
-		$sql="SELECT book_id as id,cover_photo,title FROM v_user_online_books WHERE name LIKE \"%{$searchQuery}%\" OR surname LIKE \"%{$searchQuery}%\"";
+		$sql="SELECT book_id as id,cover_photo,title FROM v_user_online_books WHERE name LIKE \"%{$searchQuery}%\" OR surname LIKE \"%{$searchQuery}%\" LIMIT $desiredSearchNumber";
 		$onlineBooksResult=$connection->query($sql);
 
 	}
 	else if(strcmp($_GET["filter"],"publishHouse")==0){
-		$sql="SELECT ISBN,cover_photo,title FROM v_publish_house_book WHERE name LIKE \"%{$searchQuery}%\"";
+		$sql="SELECT ISBN,cover_photo,title FROM v_publish_house_book WHERE name LIKE \"%{$searchQuery}%\" LIMIT $desiredSearchNumber";
 		$offlineBooksResult=$connection->query($sql);
 		//ktu do mendojm dicka tjt po me pak fjal dua t m sjelli dicka bosh;
 		$sql="SELECT id,cover_photo,title FROM online_books WHERE id=-3";
@@ -38,25 +38,18 @@ $searchQuery=$_GET["searchQuery"];
 		
 	}
 
-	while ($desiredSearchNumber>0) {
-			if($libratOnline=$onlineBooksResult->fetch_assoc()){
-					$pageResult.="<a href='book.php?id=".$libratOnline["id"]."'><div>"."<img src='/BibliotekaOnline/images/onlineBooks/".$libratOnline["cover_photo"]."'>"."<span>".$libratOnline["title"]."<br><br></span></div><hr></a>";
-						$desiredSearchNumber--;
-			}
-			else{
-				break;
-			}
-		
+	while ($libratOnline=$onlineBooksResult->fetch_assoc()) {
+			$pageResult.="<a href='book.php?id=".$libratOnline["id"]."'><div>"."<img src='/BibliotekaOnline/images/onlineBooks/".$libratOnline["cover_photo"]."'>"."<span>".$libratOnline["title"]."<br><br></span></div><hr></a>";
+				$desiredSearchNumber--;
 	}
 
-	while ($desiredSearchNumber>0) {
-		if($libratOffline=$offlineBooksResult->fetch_assoc()){
-			$pageResult.="<a href='book.php?isbn=".$libratOffline["ISBN"]."'><div>"."<img src='/BibliotekaOnline/images/books/".$libratOffline["cover_photo"]."'>"."<span>".$libratOffline["title"]."<br><br></span></div><hr></a>";
+	while ($libratOffline=$offlineBooksResult->fetch_assoc()) {
+		if($desiredSearchNumber==0){
+				break;
+			}
+		$pageResult.="<a href='book.php?isbn=".$libratOffline["ISBN"]."'><div>"."<img src='/BibliotekaOnline/images/books/".$libratOffline["cover_photo"]."'>"."<span>".$libratOffline["title"]."<br><br></span></div><hr></a>";
 			$desiredSearchNumber--;
-		}
-		else{
-			break;
-		}
+
 		
 	}
 
